c3cde6ca54008430d3e3d84dcaa33f61006a6a01
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-10-16 16:49:10 -0500

    added users model, added user error handler

---
+++ users/users-model.js
@@ -0,0 +1,30 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+	add,
+	find,
+	findBy,
+	findById,
+	remove,
+  };
+  
+  async function add(user) {
+	const [id] = await db('users').insert(user, 'id');
+	return db('users').where({ id }).first();
+  }
+  
+  function find() {
+	return db('users');
+  }
+  
+  function findBy(filter) {
+	return db('users').where(filter);
+  }
+  
+  function findById(id) {
+	db('users').where({ id }).first();
+  }
+  
+  function remove(id) {
+	return db('users').where({ id }).del();
+  }
