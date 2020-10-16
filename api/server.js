dd05890daa61e3964275649a5d0c91a8b921c506
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-10-16 16:50:10 -0500

    authentication added

--- api/server.js
+++ api/server.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const errHandler = require('./errHandler.js');
+const authRouter = require('../auth/auth-router.js');
 
 const server = express();
 
+server.use(helmet());
 server.use(express.json());
+server.use(cors());
+server.use(logger);
+
+server.use('/api/auth', authRouter);
 
 server.get('/', (req, res) => {
 	res.send('Server is running!');
 });
+
+function logger(req, res, next) {
+	console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('host')}`);
+	next();
+}
+
+server.use(errHandler);
 
 module.exports = server;
--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,22 @@
-const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/users";
-
+const pgConnection = process.env.DATABASE_URL || 'postgresql://postgres@localhost/users';
 
 module.exports = {
 	development: {
 		client: 'sqlite3',
 		useNullAsDefault: true,
-		connection: { filename: './data/african.db3' },
+		connection: {
+			filename: './data/african.db3',
+		},
+		pool: {
+			afterCreate: (conn, done) => {
+				conn.run('PRAGMA foreign_keys = ON', done);
+			},
+		},
 		migrations: {
 			directory: './data/migrations',
 		},
 		seeds: {
 			directory: './data/seeds',
-		},
-		pool: {
-			afterCreate: (conn, done) => {
-				// runs after a connection is made to the sqlite engine
-				conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
-			},
 		},
 	},
 
@@ -24,26 +24,20 @@ module.exports = {
 		client: 'sqlite3',
 		useNullAsDefault: true,
 		connection: {
-			filename: './data/testing/african-test.db3',
+			filename: './data/african-test.db3',
 		},
 		migrations: {
-			directory: './data/testing/migrations',
+			directory: './data/migrations',
 		},
 		seeds: {
-			directory: './data/testing/seeds',
-		},
-		pool: {
-			afterCreate: (conn, done) => {
-				// runs after a connection is made to the sqlite engine
-				conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
-			},
+			directory: './data/seeds',
 		},
 	},
 
 	production: {
 		client: 'pg',
 		useNullAsDefault: true,
-    connection: pgConnection,
+		connection: pgConnection,
 		migrations: {
 			directory: './data/migrations',
 		},
@@ -51,10 +45,8 @@ module.exports = {
 			directory: './data/seeds',
 		},
 		pool: {
-			afterCreate: (conn, done) => {
-				// runs after a connection is made to the sqlite engine
-				conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
-			},
+			min: 2,
+			max: 10,
 		},
 	},
 };
