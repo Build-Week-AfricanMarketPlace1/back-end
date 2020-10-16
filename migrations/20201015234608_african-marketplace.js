d15b1ee40172b8ce3796a7e24fdfa736f6c203b9
Britney Parkerson <57121314+BritneyParkerson@users.noreply.github.com>
2020-10-16 00:28:19 -0400

    Added Migrations and Seeds

---
+++ migrations/20201015234608_african-marketplace.js
@@ -0,0 +1,35 @@
+exports.up = function(knex) {
+    return knex.schema
+    .createTable('users', users => {
+      users.increments();
+      users.string('username', 255).unique().notNullable();
+      users.string('email_address', 255).unique().notNullable();;
+      users.string('password', 255).notNullable().unsigned();
+      users.string('country', 255);
+      users.string('image_url', 255);
+  })
+  .createTable('categories', categories => {
+      categories.increments();
+      categories.string('category_name', 128).unique().notNullable();
+  })
+      .createTable('items', items => {
+          items.increments();
+          items.string('item_name', 128).unique().notNullable();
+          items.string('image_url');
+          items.float('price').notNullable().unsigned();
+          items.string('description');
+          items.string('country');
+          items.string('city');
+          items.string('address');
+          items.integer('zip_code');
+          items.timestamp('created_at').defaultTo(knex.fn.now());
+          items.integer('user_id').notNullable().references('user_id');
+          items.integer('category_id').notNullable().references('category_id')
+      })
+  };
+  
+  exports.down = function(knex) {
+      return knex.schema.dropTableIfExists('users')
+       .dropTableIfExists('catergories')
+      .dropTableIfExists('items')
+  };
