9e6515afb443aad6ef9f7aba288b9cb0337dc6f9
Britney Parkerson <57121314+BritneyParkerson@users.noreply.github.com>
2020-10-26 00:32:59 -0400

    corrected Tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,14 +1,18 @@
 const server = require('./server.js');
 const request = require('supertest');
 
-describe('server', () => {
-	it('Should return 200', async () => {
+const db = require('../data/dbConfig.js');
+const testUser = { username: 'test', email: 'email@emailtest.com', password: 'password' };
+
+describe('server.js', () => {
+	it('it should return status code 200', async () => {
 		const res = await request(server).get('/');
 		expect(res.status).toBe(200);
 	});
 
-	it("Should return 'text/html in charset utf-8'", async () => {
+	it("it should return 'MOTD'", async () => {
+        const MOTD = process.env.MOTD;
 		const res = await request(server).get('/');
-		expect(res.get('Content-Type')).toEqual('text/html; charset=utf-8');
+		expect(res.get('Content-Type')).toBe(MOTD);
 	});
 });
